40b2c6b43a063c6161c585c286b8ae41a9e20297
Schneems <richard.schneeman+foo@gmail.com>
2025-01-16 09:48:42 -0600

    Emit logging format in  docs from functions

---
+++ src/docs/global_setup.rs
@@ -0,0 +1,6 @@
+
+use bullet_stream::global::print;
+#
+# let temp = tempfile::tempdir().unwrap();
+# let path = temp.path().join("log");
+# bullet_stream::global::set_writer(std::fs::File::create(&path).unwrap());
